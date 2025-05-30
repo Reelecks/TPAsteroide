3d4d6ddc77dc3ee4172d1079bb143e41e384fc65
Tanguy <tanguymorel.pro@gmail.com>
2025-05-30 10:39:14 +0200

    add: AlertBanner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import AlertBanner from "./components/AlertBanner";
 import MeteorList from "./components/MeteorList";
 import TopBar from "./components/TopBar";
 import dataJson from "./FakeDatas.json";
 
 function App() {
+  const threatCount = dataJson.filter((obj) => obj.menace).length;
   return (
     <div id="app">
       <TopBar />
+      <AlertBanner threatCount={threatCount} />
       <MeteorList data={dataJson} />
     </div>
   );
