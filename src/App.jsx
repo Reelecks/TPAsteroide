8a5a55132514cde91844b850376da3efa7245fbc
Tanguy <tanguymorel.pro@gmail.com>
2025-05-30 10:18:08 +0200

    add: MeteorList with fake datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import MeteorList from "./components/MeteorList";
 import TopBar from "./components/TopBar";
+import dataJson from "./FakeDatas.json";
 
 function App() {
   return (
     <div id="app">
       <TopBar />
+      <MeteorList data={dataJson} />
     </div>
   );
 }
